581cc9b552378b167c9c205748e147d80b4e12eb
fartoot <fartoot1337@gmail.com>
2024-01-17 03:55:51 +0100

    improve ui

--- Views/index.php
+++ Views/index.php
@@ -23,17 +23,33 @@
         <div class="flex-auto bg-slate-300 p-4">
             <div class="">
                 <div class="mb-8 mx-auto">
-                    <a class="mx-auto block w-2/5 px-12 py-3 text-sm font-medium text-white bg-slate-600 border border-slate-600 rounded-full active:text-slate-500 hover:bg-transparent hover:text-slate-600 focus:outline-none focus:ring" href="/new">New Chat</a>
+                    <a class="shadow-md mx-auto block w-2/5 text-center py-3 text-sm font-medium text-white bg-slate-600 border border-slate-600 rounded-full active:text-slate-500 hover:bg-transparent hover:text-slate-600 focus:outline-none focus:ring" href="/new">New Chat</a>
                 </div>
-                <div class="inline-block flex justify-center">
-                    <button class="w-3/5 border-2 border-slate-400 rounded my-2 py-4">22/1/2024</button>
+                <div class="text-center ">
+                    <a href="#1" class="mx-auto text-left block w-3/5 hover:ring-2 focus:ring-2 hover: ring-slate-400 rounded-lg my-2 py-4 px-4 hover:bg-white focus:bg-white"> 
+                        <div class="flex justify-around">
+                            <span class="text-slate-800">22/01/2024</span>
+                            <svg class="fill-slate-600 sm:max-md:hidden" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg>
+                        </div>    
+                    </a>
+                    <a href="#1" class="mx-auto text-left block w-3/5 hover:ring-2 focus:ring-2 hover: ring-slate-400 rounded-lg my-2 py-4 px-4 hover:bg-white focus:bg-white"> 
+                        <div class="flex justify-around">
+                            <span class="text-slate-800">22/01/2024</span>
+                            <svg class="fill-slate-600 sm:max-md:hidden" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg>
+                        </div>    
+                    </a>
+                    <a href="#1" class="mx-auto text-left block w-3/5 hover:ring-2 focus:ring-2 hover: ring-slate-400 rounded-lg my-2 py-4 px-4 hover:bg-white focus:bg-white"> 
+                        <div class="flex justify-around">
+                            <span class="text-slate-800">22/01/2024</span>
+                            <svg class="fill-slate-600 sm:max-md:hidden" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg>
+                        </div>    
+                    </a>
+
                 </div>
             </div>
         </div>
-        <div class="flex-auto w-3/5 bg-slate-50 py-5 px-5 lg:px-12 flex justify-center">
+        <div class="flex-auto w-8/12 bg-slate-50 py-5 px-5 lg:px-12 flex justify-center">
             <div class="lg:w-3/5 flex flex-wrap">
-                <!-- <p>ChatGPT 4</p> -->
-
                 <div class="flex items-center justify-center">
                         <div>
                             <select id="chatgpt"  onchange="selectedChagpt()" class="space-x-40 bg-inherit rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-slate-400">
@@ -58,7 +74,7 @@
                 <div class="w-full">
                     <form action="create" method="post">
                         <input type="hidden" name="chagpt" id="selectedChagpt" value="<?= $chatgpt ?>" >
-                        <input type="text" class="border-2 border-slate-400 rounded-full h-16 w-full px-8" name="prompt" id="" placeholder="prompt">
+                        <input type="text" class="shadow-lg border-2 border-slate-300 hover:border-slate-400 active:border-slate-400 rounded-full h-16 w-full px-8" name="prompt" placeholder="Your prompt ...">
                     </form>
                 </div>
             </div>
