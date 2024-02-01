a5db50a7a3619afb17d73605b2bd6b96441b0777
fartoot <fartoot1337@gmail.com>
2024-02-01 01:02:17 +0100

    fixed and improved ui and added submit button

--- Views/index.php
+++ Views/index.php
@@ -6,37 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" href="/resources/style/style.css">
-
-        <style>
-                /* code {
-                        word-break: break-work;
-                } */
-
-                /* 
-                .language-bash{
-                        background-color:antiquewhite;
-                } */
-
-                /* pre {
-                        background-color: #1e1e1e;
-                        color: white;
-                        padding: 10px;
-                        word-break: break-all;
-                        border-radius: 12px;
-                        padding: 15px;
-                        margin: 20px 0px !important;
-
-                } */
-/* 
-                p > * {
-                        word-break: break-all;
-                } */
-        </style>
 </head>
 
 <body>
         <div class="flex h-screen">
-                <div id="sidebar" class="flex-auto md:w-4/12 bg-slate-300 absolute md:relative h-screen md:h-full w-full p-4 hidden md:inline">
+                <div id="sidebar" class="flex-auto md:w-3/12 bg-slate-300 absolute md:relative h-screen md:h-full w-full p-4 hidden md:inline">
                         <div class="h-full flex flex-col justify-evenly md:justify-around">
                                 <div class="mx-auto">
                                         <a class="inline shadow-md mx-auto  px-6 py-4 text-sm font-medium text-white bg-slate-600 border border-slate-600 rounded-full active:text-slate-500 hover:bg-transparent hover:text-slate-600 focus:outline-none focus:ring" href="/new">
@@ -48,7 +22,7 @@
                                         <div class="my-5">
                                                 <h3 class="flex items-center">
                                                         <span aria-hidden="true" class="flex-grow bg-slate-400 rounded h-0.5"></span>
-                                                        <span class="inline-block px-4 py-1 text-sm font-medium text-center text-slate-600 bg-slate-400 rounded-full">
+                                                        <span class="inline-block px-4 py-1 text-sm font-medium text-center text-slate-700 bg-slate-400 rounded-full">
                                                                 History
                                                         </span>
                                                         <span aria-hidden="true" class="flex-grow bg-slate-400 rounded h-0.5"></span>
@@ -76,11 +50,12 @@
 
                                 <div class="hidden md:inline">
                                         <img class="w-1/2 xl:w-1/3 mx-auto m-3 rounded-lg" src="/imgs/qrcode.svg" alt="">
+                                        <p class="text-center -m-2 font-mono text-slate-900">scan me</p>
                                 </div>
                         </div>
                 </div>
                 <div class="bg-slate-50 w-full py-5 px-5 lg:px-12 flex justify-center">
-                        <div class="lg:w-3/5 flex flex-wrap">
+                        <div class="lg:w-4/5 xl:w-3/5 flex flex-wrap">
                                 <div class="mb-3 md:mb-0 flex justify-between w-full">
                                         <div class="flex items-center justify-center">
                                                 <div>
@@ -109,7 +84,7 @@
                                 <div id="content" class="h-5/6 w-full overflow-y-auto ">
                                         <?php
                                         foreach ($data["answers"] as $answer) {
-                                                echo "<div class='block bg-white p-3 mb-2 border-2 border-slate-300 rounded-md' >";
+                                                echo "<div class='bg-white p-3 mb-3 space-y-1.5 border-2 border-slate-300 rounded-md'>";
                                                 echo "<p class='mb-2 text-base font-bold'>" . $answer["question"] . "</p>";
                                                 echo $Parsedown->text($answer["answer"]);
                                                 echo "</div>";
@@ -125,9 +100,17 @@
                                 </div>
 
                                 <div class="w-full">
-                                        <form action="/create/<?= $id ?>" method="post" onSubmit="return loading()">
+                                        <form action="/create/<?= $id ?>" method="post" onSubmit="return loading()" class="flex">
                                                 <input type="hidden" name="chagpt" id="selectedChagpt" value="<?= $chatgpt ?>">
-                                                <input type="text" class="shadow-lg border-2 border-slate-300 hover:border-slate-400 active:border-slate-400 rounded-full h-16 w-full px-8" name="prompt" placeholder="Your prompt ...">
+                                                <div class="relative w-full">
+                                                        <input type="text" class="shadow-lg border-2 pr-16 border-slate-300 hover:border-slate-400 active:border-slate-400 rounded-full h-16 w-full px-8" name="prompt" placeholder="Your prompt ...">
+                                                        <button type="submit" class="aspect-square  p-2 text-white absolute inset-y-0 right-0 m-2   font-medium rounded-full text-sm  bg-slate-600 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">
+                                                                
+                                                                <svg class="w-8  " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                                                        <path d="M21.707 2.293a1 1 0 0 0-1.069-.225l-18 7a1 1 0 0 0 .145 1.909l8.379 1.861 1.862 8.379a1 1 0 0 0 .9.78L14 22a1 1 0 0 0 .932-.638l7-18a1 1 0 0 0-.225-1.069zm-7.445 15.275L13.1 12.319l2.112-2.112a1 1 0 0 0-1.414-1.414L11.681 10.9 6.432 9.738l12.812-4.982z" style="fill:#fff" data-name="Share" />
+                                                                </svg>
+                                                        </button>
+                                                </div>
                                         </form>
                                 </div>
                         </div>
